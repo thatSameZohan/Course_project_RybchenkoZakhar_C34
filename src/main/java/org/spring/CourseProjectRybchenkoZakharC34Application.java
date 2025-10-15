5c4d41c51aa40cdac8062c613be7363c074523cf
thatSameZohan <king31121996@gmail.com>
2025-10-15 15:01:48 +0300

    init commit

---
+++ src/main/java/org/spring/CourseProjectRybchenkoZakharC34Application.java
@@ -0,0 +1,13 @@
+package org.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseProjectRybchenkoZakharC34Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseProjectRybchenkoZakharC34Application.class, args);
+    }
+
+}
